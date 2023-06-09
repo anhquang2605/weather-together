6e3aa616269079f833dd7f144070962b9b0fd215
Anh Quang Chu <anhquang2605@gmail.com>
2023-06-08 21:53:28 -0700

    added user and userSlice, need to figure out how to use the user:

--- store/store.ts
+++ store/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import weatherReducer from './features/weather/weatherSlice';
+import userReducer from './features/user/userSlice';
 export const store = configureStore({
     reducer:{
         weather:weatherReducer,
+        user:userReducer
     }
 })
 
