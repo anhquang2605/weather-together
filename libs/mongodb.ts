afd21e0bde2fa4c938ed4869e34d2138e607345d
anhquang2605 <anhquang2605@gmail.com>
2023-06-12 20:36:02 -0700

    added change to lib folder, moved connect db function to mongodb.js

---
+++ libs/mongodb.ts
@@ -0,0 +1,26 @@
+import * as mongoDB from "mongodb";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const PW = process.env.MONGO_PW;
+const USER = process.env.MONGO_USER;
+const DB = process.env.MONGO_DB;
+const uri = `mongodb+srv://${USER}:${PW}@${DB}.8lqcwfi.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+let db: mongoDB.Db;
+export async function connectDB() {
+    try{
+        if(db) return db;
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        db = await client.db(DB);  
+        return db;
+    } catch (e) {
+        console.log(e);
+    }
+}
--- libs/users.ts
+++ libs/users.ts
@@ -1,31 +1,11 @@
 import * as mongoDB from "mongodb";
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const PW = process.env.MONGO_PW;
-const USER = process.env.MONGO_USER;
-const DB = process.env.MONGO_DB;
-const uri = `mongodb+srv://${USER}:${PW}@${DB}.8lqcwfi.mongodb.net/?retryWrites=true&w=majority`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-let db: mongoDB.Db;
-export async function connectDB() {
-    try{
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        db = await client.db(DB);  
-    } catch (e) {
-        console.log(e);
-    }
-}
+import { connectDB } from "./mongodb";
 //UTILS
 export async function getUserIds(){//CHANGES
     try{
-        await connectDB();
+        let db = await connectDB() 
+        if(!db) return [];
         const usersCollection: mongoDB.Collection = await db.collection('users');
         const ids : string[] = (await usersCollection.find({}, { projection: { _id: 1 } }).toArray()).map(obj => obj._id.toString());
         //in the future, we will have a function that retrieve user ids from the database
@@ -38,19 +18,20 @@ export async function getUserIds(){//CHANGES
             }
         });
         return  paths;
-    } finally {
-        await client.close();
+    } catch(e) {
+        console.log(e);
     }
     return [];
 }
 
 export async function getUserData(id: string){
     try{
-        await connectDB();
+        let db = await connectDB() 
+        if(!db) return null;
         const usersCollection: mongoDB.Collection = await db.collection('users');
         const user = await usersCollection.findOne({ _id : new mongoDB.ObjectId(id) });
         return user;
-    } finally {
-        await client.close();
+    } catch(e) {
+        console.log(e);
     }
 }
