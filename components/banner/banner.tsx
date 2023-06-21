8d97ab2ab5145a8a5ae90f7b3d0b75e89cf5c9ec
Anh Quang Chu <anhquang2605@gmail.com>
2023-06-21 15:16:58 -0700

    added way to extract piece of data from api

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,3 +1,4 @@
+import { use, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 interface BannerProps {
@@ -5,6 +6,7 @@ interface BannerProps {
 export default function Banner() {
     const user = useSelector((state: any) => state.user);
     const {location} = user?.data ?? {location: undefined};
+    const currentCondition = useSelector((state: any) => state.weatherDaily.currentCondition);
     return (
         <>
             <div className="banner drop-shadow  bg-indigo-800 mb-4 rounded grow order-first w-full p-4">
@@ -12,6 +14,9 @@ export default function Banner() {
 
                     <h1 className="banner__title">Good {user?.data?.username ?? ""} Welcome to <span className="banner__title--highlight">Weather</span> App</h1>
                     <p className="banner__description">{/*Message of the day*/}When the sun come, shine brighter</p>
+                    <p>
+                        It is a {currentCondition.icon}
+                    </p>
                 </div>
             </div>
         </>
--- store/features/weather/weatherSlice.tsx
+++ store/features/weather/weatherSlice.tsx
@@ -11,11 +11,13 @@ const weatherDailyAdapter = createEntityAdapter<Value>({
 const initialDailyState = weatherDailyAdapter.getInitialState({
     status: 'idle',
     error: null,
+    currentCondition: null,
 
 })
 const initialHourlyState = weatherHourlyAdapter.getInitialState({
     status: 'idle',
     error: null,
+    currentCondition: null,
 })
 
 const API_HOST = process.env.NEXT_PUBLIC_WEATHER_API_HOST;
@@ -31,22 +33,34 @@ const options = {
 export const fetchWeatherHourly = createAsyncThunk('weather/fetchWeatherHourly', async (city: string) => {
     const response = await fetch(`${API_BASE_URL}/forecast?location=${city}&unitGroup=us&aggregateHours=1&contentType=json&shortColumnNames=1`, options);
     const data = await response.json();
-    const hourlyData = data?.locations?.[city]?.values;
+    const theCity = data?.locations?.[city];
+    const hourlyData = theCity.values;
     const withIds= hourlyData.map((e: Value,index: number) => ({
         ...e,
         id: index
-}))
-return withIds;
+    }))
+    const currentCondition = theCity.currentConditions;
+
+    return {
+        currentCondition,
+        dataWithIds: withIds
+    };
 });
 export const fetchWeatherDaily = createAsyncThunk('weather/fetchWeatherDaily', async (city: string) => {
     const response = await fetch(`${API_BASE_URL}/forecast?location=${city}&unitGroup=us&aggregateHours=24&contentType=json&shortColumnNames=1`, options);
     const data = await response.json();
-    const dailyData = data?.locations?.[city]?.values;
+    const theCity = data?.locations?.[city];
+    const dailyData = theCity.values;
     const withIds= dailyData.map((e: Value,index: number) => ({
-            ...e,
-            id: index
+        ...e,
+        id: index
     }))
-    return withIds;
+    const currentCondition = theCity.currentConditions;
+
+    return {
+        currentCondition,
+        dataWithIds: withIds
+    };
 })
 const weatherHourlySlice = createSlice({
     name: 'weatherHourly',
@@ -56,7 +70,8 @@ const weatherHourlySlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(fetchWeatherHourly.fulfilled, (state, action) => {
             
-            weatherHourlyAdapter.upsertMany(state, action.payload)
+            weatherHourlyAdapter.upsertMany(state, action.payload.dataWithIds)
+            state.currentCondition = action.payload.currentCondition;
         });
     }
 });
@@ -68,8 +83,8 @@ const weatherDailySlice = createSlice({
     extraReducers: (builder) => {
         
         builder.addCase(fetchWeatherDaily.fulfilled, (state, action) => {
-            console.log(action.payload)
-            weatherDailyAdapter.upsertMany(state, action.payload);
+            weatherDailyAdapter.upsertMany(state, action.payload.dataWithIds);
+            state.currentCondition = action.payload.currentCondition;
         });
     }
 });
