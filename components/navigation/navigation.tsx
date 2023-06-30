f39940b1f5e01ff829b55a093ec08dd7d4a5dc78
Anh Quang Chu <anhquang2605@gmail.com>
2023-06-29 18:11:31 -0700

    added style to nav bar

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
 import UserMenu from "./usermenu/usermenu"
+import {FaNewspaper} from "react-icons/fa"
+import {IoCloudyNight} from "react-icons/io5"
 export default function Navigation() {
     return(
-        <div className="bg-indigo-800 grow-0 drop-shadow-lg rounded p-4 shrink">
-            <div>
-                <span>Icon</span>
+        <div className="transition bg-violet-950 grow-0 drop-shadow-lg rounded p-4 shrink">
+            <div className="flex flex-row items-center border-b border-slate-500 pb-4">
+                <IoCloudyNight className="w-6 h-6 mr-4"></IoCloudyNight>
+                <h3 className="font-semibold">Weather Together</h3>
             </div>
+
             <ul>
-                <li><Link href={`/`}>Home </Link></li>
+                <li><Link className="nav-item" href={`/`}> <FaNewspaper/> <span>Home</span> </Link></li>
                 <li>
                     <UserMenu />
                 </li>
--- components/navigation/usermenu/usermenu.tsx
+++ components/navigation/usermenu/usermenu.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link"
 import { useSelector } from "react-redux"
+import {BsPersonVcardFill} from "react-icons/bs"
+import {IoEnter, IoExit, IoPersonAdd} from "react-icons/io5"
 export default function UserMenu() {
     const user = useSelector((state: any) => state.user);
     return (
         <div className="flex flex-col">
-            <span>Small avatar</span>
             {user.data ?
                 <>
-                    <Link href="/authentication/logout">Log out</Link>
-                    <Link href={"/userprofile/" + (user.data ? user.data.username : "")}>User profile</Link>
+                    <Link className="nav-item" href="/authentication/logout"><IoExit></IoExit><span>Log out</span></Link>
+                    <Link className="nav-item" href={"/userprofile/" + (user.data ? user.data.username : "")}><BsPersonVcardFill></BsPersonVcardFill><span>User profile</span></Link>
                 </>
                 :
                 <>
-                    <Link href="/authentication/login">Log in</Link>
-                    <Link href="/authentication/register">Register</Link>
+                    <Link className="nav-item" href="/authentication/login"><IoEnter></IoEnter><span>Log in</span></Link>
+                    <Link className="nav-item" href="/authentication/register"><IoPersonAdd></IoPersonAdd><span>Register</span></Link>
                 </>
             }
 
