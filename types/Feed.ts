a7115c2fd04844fff41b2e8727f2da95d0899832
Anh Quang Chu <anhquang2605@gmail.com>
2023-07-31 22:13:48 -0700

    added minor change to feed datastructure to allow storing reaction type as well as specifying which fields are optional

--- types/Feed.ts
+++ types/Feed.ts
@@ -1,12 +1,14 @@
 export interface Feed {
     _id?: string;
-    title: string;
-    createdDate?: Date;
+    title?: string;
+    createdDate: Date;
     username: string;//user who created the feed
+    relatedUser?: string;//user who is related to the feed, for example, the user who is tagged in a post
     type: string;//type of feed, comment, react, post, friend becamecreate...
+    reactionType?: string;
     activityId: string;//id of activity, comment, react, post...
-    targetType: string;//type of target, post, comment...
-    targetId: string;//id of target post, comment
+    targetType?: string;//type of target, post, comment...
+    targetId?: string;//id of target post, comment
     targetParentId?: string;//id of parent post if targetType is a comment, will be '', if targetType is a post
 
 }
