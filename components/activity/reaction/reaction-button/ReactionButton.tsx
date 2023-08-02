23a0f6a88652fb46a1136da7712fc3dfc47945d7
Anh Quang Chu <anhquang2605@gmail.com>
2023-08-02 12:22:07 -0700

    added some note to reaction button component for next steps

--- components/activity/reaction/reaction-button/ReactionButton.tsx
+++ components/activity/reaction/reaction-button/ReactionButton.tsx
@@ -13,6 +13,21 @@ interface ReactionFetchParams{
     username: string;
     targetId: string;
 }
+/*
+    This component is used to display the reaction button
+    To manage the reaction state, this component will fetch the initial reaction from the server
+    and then update the state accordingly
+    If the user has reacted, the button will be highlighted, there would be reaction returned by the server but only when the reaciton name is not empty
+
+    Note for next steps:
+    - only set expireAt field when the user revoke the reaction by setting the name to ''
+    - when the user react, the expireAt will be remove by using the unset operator in mongodb
+    - we keep the reaction alive for 1 day if ther user has not react again
+    - on client side:
+        + We debounce the reaction update to 1 second to prevent user from spamming the reaction button while User can still interact with the internal state of the button for better UX
+        + We use optimistic UI to update the reaction state, this means that we update the reaction state before the server response, if the server response is not successful, we revert the reaction state back to the previous state   
+
+*/
 export default function ReactionButton(
     {
         username,
@@ -28,7 +43,7 @@ export default function ReactionButton(
         const reponse = await fetchFromGetAPI(path, params);
         const reaction = await reponse.json();
         //reaction.name == '' means no reaction, this happen when the user reacted and then unreacted
-        
+
         if(reaction){
             setReaction(reaction);
             if(reaction.name != ''){
