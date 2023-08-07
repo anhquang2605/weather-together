72109642acf114e83dbf865cbdca4af43a59e209
anhquang2605 <anhquang2605@gmail.com>
2023-08-07 15:13:37 -0700

    working the suggestion for emoji, need to figure out the exact position of the cursor to reveal the suggestion box

--- components/activity/comment/comment-form/CommentForm.tsx
+++ components/activity/comment/comment-form/CommentForm.tsx
@@ -1,10 +1,13 @@
 
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import { insertToPostAPI, uploadFileToPostAPI } from '../../../../libs/api-interactions';
 import style from './comment-form.module.css';
 import MiniAvatar from '../../mini-avatar/MiniAvatar';
 import {IoCamera,IoSend} from 'react-icons/io5';
 import EmojiSelector from '../../text-editing/emoji-selections/EmojiSelector';
+import IntextSuggestion from '../../text-editing/intext-suggestion/IntextSuggestion';
+import { EMOJIS } from '../../../../constants/emojis';
+import { Emoji } from '../../../../types/Emoji';
 interface ErrorMessage {
     message: string,
     type: string //picture-attachment, content-length
@@ -23,12 +26,33 @@ export default function CommentForm({targetId, username, targetLevel = 0, postId
     const [content, setContent] = useState('');
     const [isSending, setIsSending] = useState(false);
     const [pictureAttached, setPictureAttached] = useState(false);
-    const [currentCursorPosition, setCurrentCursorPosition] = useState(0);
     const [picture, setPicture] = useState<File>();
     const [previewPictureURL, setPreviewPictureURL] = useState<string | null>('');
     const [errorMessages, setErrorMessages] = useState<ErrorMessage[]>([]);
     const [validContentLength, setValidContentLength] = useState(false); //min should be 1 word
     const [s3PictureURL, setS3PictureURL] = useState<string | null>(null);
+    //intext suggestions state, used to provide props for intext suggestion component
+    const [currentCursorPosition, setCurrentCursorPosition] = useState(0);
+    const [suggestions, setSuggestions] = useState<Emoji[]>(EMOJIS);
+    const [revealEmojiSuggestions, setRevealEmojiSuggestions] = useState(false);
+    const [emojiSuggestionTerm, setEmojiSuggestionTerm] = useState(''); //the term that is used to filter the suggestions
+    const contentTextAreaRef = useRef<HTMLTextAreaElement | null>(null);
+    const triggerChar = ':';
+    const renderSuggestions = (suggestions: Emoji[]) => {
+        return(
+            suggestions.map((suggestion, index) => (
+                <div className={style['emoji-selection-item']}>
+                    {suggestion.emoji}
+                </div>
+            ))
+        )
+    }
+    const suggestionsContainerClassName = style['emoji-selection-container'];
+    const handleEmojiSuggestionClick = (emoji: Emoji) => {
+        //replace the term with the emoji
+        const newContent = content.replace(`:${emojiSuggestionTerm}`, emoji.emoji);
+        setContent(newContent);        
+    }
     const handlePictureInptChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
         const reader = new FileReader();
@@ -82,8 +106,29 @@ export default function CommentForm({targetId, username, targetLevel = 0, postId
             setValidContentLength(false);
         }
     }
-    const handleKeyUpTextAree = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        
+    //reveal emoji suggestions when trigger char is typed
+    const handleKeyUpTextArea = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        setCurrentCursorPosition(e.currentTarget.selectionStart || 0);
+        if(e.key === triggerChar) {
+            setRevealEmojiSuggestions(true);
+        } 
+        if(e.key === " "){
+            setRevealEmojiSuggestions(false);
+        }
+      
+    }
+    //stop revealing when backspace is pressed and the trigger char is deleted
+    const handleKeyDownTextArea = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === "Backspace") {
+            
+            //get character at current cursor position
+            const currentContent = contentTextAreaRef.current?.value;
+            const currentCursorPosition = e.currentTarget.selectionStart || 0;
+            const currentChar = currentContent?.[currentCursorPosition - 1];
+            if(currentChar === triggerChar) {
+                setRevealEmojiSuggestions(false);
+            }
+        }
     }
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -160,9 +205,29 @@ export default function CommentForm({targetId, username, targetLevel = 0, postId
         <div className={style['comment-form']}>
             <MiniAvatar profilePicturePath={userProfilePicturePath} size="large"/>
             <div className={style['text-box']}>
-                <textarea rows={1} className={style['comment-form__content']} placeholder="Write a comment..." value={content} onChange={handleContentChange}></textarea>
+                <textarea 
+                    className={style['comment-form__content']} 
+                    placeholder="Write a comment..." 
+                    value={content} 
+                    onChange={handleContentChange}
+                    onKeyUp={handleKeyUpTextArea}
+                    onKeyDown={handleKeyDownTextArea}
+                    ref={contentTextAreaRef}
+                ></textarea>
+                {revealEmojiSuggestions && <IntextSuggestion<Emoji>
+                    reveal={revealEmojiSuggestions}
+                    setReveal={setRevealEmojiSuggestions}
+                    triggerChar={triggerChar}
+                    suggestions={suggestions}
+                    suggestionJSX={renderSuggestions}
+                    inputRef={contentTextAreaRef}
+                    currentCursorPosition={currentCursorPosition}
+                    term={emojiSuggestionTerm}
+                    handleSuggestionClick={handleEmojiSuggestionClick}
+                    suggestionContainerClassName={suggestionsContainerClassName}
+                />}                
+                
                 <div className={style['control-group']}>
-
                     <div className={style['attachment-group']}>
                         <div className={style['picture-attachment']}>
                             <label title="Attach a picture" htmlFor="picture-comment-upload" className={style['control-btn']}>
--- components/activity/text-editing/intext-suggestion/IntextSuggestion.tsx
+++ components/activity/text-editing/intext-suggestion/IntextSuggestion.tsx
@@ -1,16 +1,52 @@
-
+import React from "react";
+import style from "./intext-suggestion.module.css";
+import { useEffect } from "react";
+import { current } from "@reduxjs/toolkit";
 interface IntextSuggestionProps<T> {
     term: string;
     handleSuggestionClick: (suggestion: T) => void;
-    setReveal: (reveal: boolean) => void;
+    setReveal: React.Dispatch<React.SetStateAction<boolean>>;
+    reveal: boolean;
     triggerChar: string; //the char that trigger the suggestion box
     suggestions: T[];
-    suggestionJSX?: (suggestion: T) => JSX.Element;
+    suggestionJSX?: (suggestions: T[]) => React.JSX.Element[];
     enableSuggestionMark?: boolean; //if true, the suggestion will be highlighted in the text
     suggestionMarkType?: string; //the pattern to mark the suggestion in the text;
-    inputRef : React.RefObject<HTMLInputElement>;
+    inputRef : React.MutableRefObject<HTMLTextAreaElement | HTMLInputElement | null>;
     currentCursorPosition: number;//parent will have to handle this
+    suggestionContainerClassName?: string;
 }
-export default function IntextSuggestion<T>({ term, handleSuggestionClick, setReveal, triggerChar, suggestions, suggestionJSX }: IntextSuggestionProps<T>) {
-    
+export default function IntextSuggestion<T extends {}>({ term, handleSuggestionClick, reveal, setReveal, triggerChar, suggestions, suggestionJSX, suggestionContainerClassName, currentCursorPosition, inputRef }: IntextSuggestionProps<T>) {
+    const getTextSizeOfRef = (ref: React.MutableRefObject<HTMLTextAreaElement | HTMLInputElement | null>) => {
+        if(ref.current){
+            return parseFloat(window.getComputedStyle(ref.current).fontSize);
+        }
+    }
+    useEffect(()=>{
+        if(reveal){
+            //set the suggestion box position based on the cursor position
+            if(inputRef && inputRef.current){
+                const inputRect = inputRef.current.getBoundingClientRect();
+                const inputWidth = inputRect.width;
+                const inputHeight = inputRect.height;
+                const inputTop = inputRect.top;
+                const inputLeft = inputRect.left;
+                const suggestionBox = document.querySelector(`.${style['intext-suggestion']}`) as HTMLDivElement;
+                const currentTextSize = getTextSizeOfRef(inputRef!);
+                console.log(currentTextSize);
+                if(suggestionBox){
+                    suggestionBox.style.top = `${inputTop + inputHeight + 4}px`;
+                    suggestionBox.style.left = `${inputLeft}px`;
+                }
+            }
+        }
+    },[reveal])
+    return(
+        <div className={
+            `${suggestionContainerClassName ??  ""} ${style['intext-suggestion']}
+            `
+        }>
+            {suggestionJSX && suggestionJSX(suggestions)}
+        </div>
+    )
 }
