38e67fa97d5adf3d6c9e59d89c2f2e4d32e02663
anhquang2605 <anhquang2605@gmail.com>
2023-11-06 19:12:42 -0800

    created the props required for control option, need to look at higher order function so that control option does not need to know the id of the content but can still trigger the handler passed into it by the parent content

--- components/activity/content-management/control-option/ControlOption.tsx
+++ components/activity/content-management/control-option/ControlOption.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import style from './control-option.module.css';
 
 interface ControlOptionProps {
-
+    title: string;
+    description?: string;
+    icon: JSX.Element;
+    onClick?: (_id:string) => void;
 }
 
-const ControlOption: React.FC<ControlOptionProps> = ({}) => {
+const ControlOption: React.FC<ControlOptionProps> = ({
+    title,
+    description,
+    icon,
+    onClick
+}) => {
     return (
-        <div className={style['control-option']}>
-            ControlOption
-        </div>
+        <button
+        className={style['control-option']}>
+            <span className={style['icon']}>
+
+            </span>
+            <span className={style['title']}>
+                {title}
+            </span>
+            <span className={style['description']}>
+                {description}
+            </span>
+        </button>
     );
 };
 
