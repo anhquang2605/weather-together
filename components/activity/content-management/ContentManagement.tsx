9e7b4205b652d14f5aa29234856421741239eea4
anhquang2605 <anhquang2605@gmail.com>
2023-11-07 18:32:00 -0800

    working on higher order function for each managment item

--- components/activity/content-management/ContentManagement.tsx
+++ components/activity/content-management/ContentManagement.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import style from './content-management.module.css';
 import {IoEllipsisHorizontal} from 'react-icons/io5';
-interface ContentManagementProps {
-
+import ControlOption from './control-option/ControlOption';
+interface ManagementItem{
+    type: string; //edit, delete, save.... for title and icons
+    handler: () => void; //handler function in a form of higher order function
+    description?: string; //description for the item
 }
 
-const ContentManagement: React.FC<ContentManagementProps> = ({}) => {
+interface ContentManagementProps {
+    items: ManagementItem[];
+}
+
+const ContentManagement: React.FC<ContentManagementProps> = ({
+    items
+}) => {
     const [reveal, setReveal] = useState<boolean>(false);
     return (
         <div className={style['content-management']}>
@@ -13,7 +22,19 @@ const ContentManagement: React.FC<ContentManagementProps> = ({}) => {
                 <IoEllipsisHorizontal/>
             </button>
             <div className={`${style["control-list"]} ${reveal ? style['reveal'] : ""}`}>
-                
+                {
+                    items.map((item, index) => {
+                        const {type, handler, description} = item;
+                        return(
+                            <ControlOption
+                                key={index}
+                                title={type}
+                                description={description}
+                                onClick={handler}
+                            />
+                        )
+                    })
+                }
             </div>
         </div>
     );
--- components/activity/content-management/control-option/ControlOption.tsx
+++ components/activity/content-management/control-option/ControlOption.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import style from './control-option.module.css';
-
+import {IoEllipsisHorizontal} from 'react-icons/io5';
+import {TbEdit, TbBookmarkPlus, TbTrash} from 'react-icons/tb';
 interface ControlOptionProps {
     title: string;
     description?: string;
-    icon: JSX.Element;
-    onClick?: (_id:string) => void;
+    onClick?: () => void;
 }
+const getIconJSX = (title: string) => {
 
+    //future consideration, make each icon animated and associated with a type of weather 
+    switch(title){
+        case 'Edit':
+            return (//fulte
+                <TbEdit/>
+            );
+        case 'Delete':
+            return (
+                <TbTrash/>
+            );
+        case 'Save':
+            return (
+                <TbBookmarkPlus/>
+            );
+        default:
+            return (
+                null
+            );
+    }
+}
 const ControlOption: React.FC<ControlOptionProps> = ({
     title,
     description,
-    icon,
     onClick
 }) => {
     return (
         <button
         className={style['control-option']}>
             <span className={style['icon']}>
-
+                {
+                    getIconJSX(title)
+                }
             </span>
             <span className={style['title']}>
                 {title}
