04bd40189b8c2e3871ae5e766013860ab23dae54
anhquang2605 <anhquang2605@gmail.com>
2023-11-15 18:40:48 -0800

    add minor change to undelete feed by ids handler

--- pages/api/feeds/undelete-feeds-by-ids.ts
+++ pages/api/feeds/undelete-feeds-by-ids.ts
@@ -5,9 +5,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const db = await connectDB();
     if (db) {
         if (req.method === 'PUT') {
-            const {feedIds} = req.body;
+            const {actIds} = req.body;
             const collection = db.collection('feeds');
-            console.log(feedIds);
             /* const result = await collection.updateMany({_id: {$in: feedIds}}, {$set: {isDeleted: false}});
             if(result.modifiedCount > 0){
                 res.status(200).json(
