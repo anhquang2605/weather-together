f33e9f438c30320c55cf02ba3b9e6f8540e51065
anhquang2605 <anhquang2605@gmail.com>
2023-11-28 16:00:20 -0800

    added delete logic for delete-urls need to test the handler

--- pages/api/s3/delete-urls.ts
+++ pages/api/s3/delete-urls.ts
@@ -13,5 +13,39 @@ const handler = nextConnect<NextApiRequest,NextApiResponse>(
       }}
 );//npm install next-connect@0.10.2
 handler.delete(async (req, res) => {
-    console.log(req);
+    const urls  = req.body;
+    if(!urls || !Array.isArray(urls)){
+        res.status(400).json({error: 'Invalid request'});
+    } else if(urls.length === 0){
+        res.status(200).json({message: 'ok'});
+    } else {
+        const keys = urls.map(url => 
+                ({
+                    Key: url.split('/').pop() as string,//to get the final string after the last slash which is the key or file name of object
+                })
+            );//to get the final string after the last slash which is the key or file name of object
+       const command = new DeleteObjectsCommand({
+              Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME as string,
+              Delete: {
+                Objects: keys,
+                Quiet: false,
+              },
+         });
+        try{
+            const {Deleted} = await s3Client.send(command);
+            if(Deleted && Deleted.length > 0){
+                res.status(200).json({success: true, deleted: Deleted});
+            }else{
+                res.status(500).json({error: 'Error deleting files'});
+            }
+        } catch (err){
+            console.log(err);
+            res.status(500).json({error: 'Error deleting files'});
+        }
+
+    }
+    res.status(200).json({message: 'ok'});
 })
+
+export default handler;
+
