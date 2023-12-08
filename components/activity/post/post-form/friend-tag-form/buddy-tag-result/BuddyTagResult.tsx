6f4a9f47566d67484f4e589a22b8510016a24e16
anhquang2605 <anhquang2605@gmail.com>
2023-12-08 13:49:51 -0800

    screen size affect fetch more for edit form, if I go full screen then fetch more is trigger, maybe inscrease the size of the lazy target by a bit like 1 px

--- components/activity/post/post-form/friend-tag-form/buddy-tag-result/BuddyTagResult.tsx
+++ components/activity/post/post-form/friend-tag-form/buddy-tag-result/BuddyTagResult.tsx
@@ -20,11 +20,8 @@ const BuddyTagResult: React.FC<BuddyTagResultProps> = ({results, fetchMore, hasM
     const [fetchState] = useLazyFetch();
     const [postUniqueId, setPostUniqueId] = useState<string>(getUniquePostId(postId));
     const handleIntersect = (entries: IntersectionObserverEntry[]) => {
-        console.log("here");
-        const target = entries[0];
-        console.log(target);
+        const target = entries.find(entry => entry.target.id.includes(postUniqueId));
         if(target && target.isIntersecting){
-            console.log(postUniqueId + " is intersecting")
             fetchMore();
         }
     }
