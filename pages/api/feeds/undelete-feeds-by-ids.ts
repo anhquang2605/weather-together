49a39ee77410149f5b418e62afcfb838b7e45c7e
anhquang2605 <anhquang2605@gmail.com>
2024-01-14 15:21:58 -0800

    working on the undelete-feeds-by-ids.ts  need to be researched more

--- pages/api/feeds/undelete-feeds-by-ids.ts
+++ pages/api/feeds/undelete-feeds-by-ids.ts
@@ -7,7 +7,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (req.method === 'PUT') {
             const {actIds, username} = req.body;
             const collection = db.collection('feeds');
-            const result = await collection.updateMany({activityId: {$in: actIds}}, {$set: {isDeleted: false}});
+            if(!actIds || !username) {
+                res.status(500).json({
+                    success: false,
+                    message: "No params provided"
+                })
+                return;
+            }
+            if(!db){
+                res.status(500).json({
+                    success: false,
+                    message: "database error"
+                })
+            }
+            const result = await collection.updateMany({activityId: {$in: actIds}}, { $pull: {
+                                results: {
+                                    hiddenBy: {
+                                        username
+                                    }
+                                }
+                            }
+                        }
+            );
             if(result.modifiedCount > 0){
                 res.status(200).json(
                     {
