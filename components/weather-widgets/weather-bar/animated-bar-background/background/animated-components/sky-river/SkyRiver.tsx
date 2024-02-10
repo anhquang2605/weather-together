dcf1380f33ac687072c8b4e72e056b0b26a4c6c4
Anh Quang Chu <anhquang2605@gmail.com>
2024-02-09 20:47:36 -0800

    working on moving clouds across screen

--- components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/SkyRiver.tsx
+++ components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/SkyRiver.tsx
@@ -5,19 +5,32 @@ import SkyLane from './sky-lane/SkyLane';
 interface SkyRiverProps {
     noOfLane?: number;
     uniqueObject?: React.ReactNode;// the object that would spawn in random lane
-
+    laneConfigs?: LaneOptionI[];//array contain config for each lane, array length should match with number of lanes
 }
-
+interface LaneOptionI{
+    speed: number;
+    zDistance: number; //how far the spawn object is from the screen, between 0 and 1, 1 is closest to the screen 
+}
 const SkyRiver: React.FC<SkyRiverProps> = ({
     noOfLane = 1,
+    laneConfigs = []
 }) => {
     const spawnLane = (laneNumber: number) => {
         const lanes = [];
+        
         for (let i = 0; i < laneNumber; i++) {
+            const {speed, zDistance} = laneConfigs[i] ?? {
+                speed: 1,
+                zDistance: 1
+            };
             lanes.push(
-                <SkyLane key={i}/>
+                <SkyLane 
+                    speed = {speed}
+                    zDistance={zDistance}
+                 key={i}/>
             );
-        }return lanes;
+        }
+        return lanes;
     };
     return (
         <div className={style['sky-river']}>
--- components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/sky-lane/SkyLane.tsx
+++ components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/sky-lane/SkyLane.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import style from './sky-lane.module.css';
 
+
+
 interface SkyLaneProps {
     widthSize?: string; // full, half, double, one-half
     zDistance?: number; // between 0 to 1
+    speed?: number;
 }
 
 const SkyLane: React.FC<SkyLaneProps> = ({
-    widthSize = "full"
+    widthSize = "full",
+    speed = 1
 }) => {
     return (
         <div className={`${style['sky-lane']} ${style[widthSize]}`}>
