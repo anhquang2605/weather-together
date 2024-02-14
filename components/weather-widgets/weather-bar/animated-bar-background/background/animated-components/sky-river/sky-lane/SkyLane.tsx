0f7a12aacd6e15a04376ad78d012f44593cebea1
anhquang2605 <anhquang2605@gmail.com>
2024-02-13 18:55:57 -0800

    finished with cloud movement, need to match speed and distance of cloud, also need to repeat cloud appearance when cloud reaches the end

--- components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/sky-lane/SkyLane.tsx
+++ components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/sky-river/sky-lane/SkyLane.tsx
@@ -78,7 +78,7 @@ const SkyLane: React.FC<SkyLaneProps> = ({
         <div ref={ref} className={`${style['sky-lane']} ${style[widthSize]}`}>
             <MovingClouds initialLeft= {objLeft} initialTop={objTop} size={objHeight} setObjWidth={setObjWidth} distance={
                 cloudDistance
-            } speed = {(1 - cloudDistance) / 10} isMoving={isHovered} />
+            } speed = {(2 + (1 - cloudDistance))/10} isMoving={isHovered} />
         </div>
     );
 };
