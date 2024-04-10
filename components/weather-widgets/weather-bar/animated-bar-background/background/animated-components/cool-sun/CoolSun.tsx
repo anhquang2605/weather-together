6144f32b357f33d839bf75f1777c8e91c42042f3
anhquang2605 <anhquang2605@gmail.com>
2024-04-10 15:52:26 -0700

    radiation should be reset

--- components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/cool-sun/CoolSun.tsx
+++ components/weather-widgets/weather-bar/animated-bar-background/background/animated-components/cool-sun/CoolSun.tsx
@@ -8,6 +8,12 @@ interface AnimePropertyType {
     [key: string]: any
 }
 const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
+    //CONSTANTS
+    const REVEAL_DURATION = 100;
+    const SUN_RADIATE_DURATION = 500;
+    const SUN_RADIATE_END_DELAY_DURATION = 250;
+
+    //STATES
     const [isInitialized, setIsInitialized] = useState<boolean>(false);
     const sunRadiationAnimationRef = useRef<AnimeInstance | undefined>(); 
     const eyeAppearAnimationRef = useRef<AnimeInstance | undefined>();
@@ -15,21 +21,27 @@ const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
         faceReveal();
         armReveal();
         sunRadiate();
+        eyeBlink();
     }
     const initializeAnimation = () => {
-        const radiantPath:SVGGeometryElement|null = document.querySelector('#sun_radiant path:first-child');
-        let pathLength = 0;
-        if(radiantPath){
-            pathLength = radiantPath.getTotalLength();
-        }
+
         animeSet('#arms path', {strokeDashoffset: anime.setDashoffset});
         animeSet('#eyes path', {strokeDashoffset: anime.setDashoffset});
         animeSet('#mouth path', {strokeDashoffset: anime.setDashoffset} );
-        animeSet('#sun_radiant path', {strokeDashoffset: pathLength / 2})
+        animeSet('#eyes_opened circle', {style: 'transform:scaleY(0)'});
+        //animeSet('#sun_radiant path', {strokeDashoffset: pathLength / 2})
         setIsInitialized(true);
     }
     const animeSet = (targets: string,   properties:AnimePropertyType) => {
         anime.set(targets, properties);
+    }
+    const getSVGPathLen = (target: string) =>  {
+        const thePath:SVGGeometryElement|null = document.querySelector(target);
+        let len = 0;
+        if(thePath){
+            len = thePath.getTotalLength();
+        }
+        return len;
     }
     const pathRevealAnimation = (paths: string[], duration: number) => {
         if(isAnimated){
@@ -51,11 +63,27 @@ const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
     }
     const armReveal = () => {
         const targets = ['#arms path']
-        pathRevealAnimation(targets, 100);
+        pathRevealAnimation(targets, REVEAL_DURATION);
     }
     const faceReveal = () => {
         const targets = ['#eyes path','#mouth path']
-        pathRevealAnimation(targets, 100);    
+        pathRevealAnimation(targets, REVEAL_DURATION);    
+    }
+    const eyeBlink = (isOpened?: boolean) => {
+        const target = '#eyes_opened circle';
+        if(isOpened || isAnimated){
+            anime({
+                targets: target,
+                duration: REVEAL_DURATION,
+                scaleY: '1',
+            })
+        }else{
+            anime({
+                targets: target,
+                duration: REVEAL_DURATION,
+                scaleY: '0',
+            })
+        }
     }
     const sunRadiate = () => {
         const targets = "#sun_radiant path";
@@ -63,15 +91,20 @@ const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
             if(isAnimated){
                 sunRadiationAnimationRef.current.play();
             }else{
+                sunRadiationAnimationRef.current.restart();
                 sunRadiationAnimationRef.current.pause();
+
             }
         }else{
+            const firstRadiationSelector = '#sun_radiant path:first-child';
+            const pathLen = getSVGPathLen(firstRadiationSelector);
             sunRadiationAnimationRef.current =  anime({
                 targets: targets,
-                strokeDashoffset: [anime.setDashoffset],
-                loop: true,
-                duration: 1000,
-                direction: 'alternate'
+                strokeDashoffset: [-pathLen, anime.setDashoffset],
+                loop: Infinity,
+                duration: SUN_RADIATE_DURATION,
+                endDelay: SUN_RADIATE_END_DELAY_DURATION,
+                easing: 'linear'
             })
         }
        
@@ -90,23 +123,23 @@ const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
     return (
         <div className={styles['cool-sun']}>
             <svg width="45%" viewBox="0 0 304 304" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g id="cool_sun" clip-path="url(#clip0_12_4)">
-                    <circle id="Ellipse 3" cx="152" cy="152" r="108" fill="yellow" fill-opacity="0.2"/>
-                    <circle id="Ellipse 4" cx="151" cy="148" r="136" fill="yellow" fill-opacity="0.2"/>
+                <g id="cool_sun" clipPath="url(#clip0_12_4)">
+                    <circle id="Ellipse 3" cx="152" cy="152" r="108" fill="yellow" fillOpacity="0.2"/>
+                    <circle id="Ellipse 4" cx="151" cy="148" r="136" fill="yellow" fillOpacity="0.2"/>
                     <g id="body">
                         <circle id="inner_body" cx="152" cy="152" r="92" fill="#EDA10D"/>
                         <circle id="outer_body" cx="151.5" cy="151.5" r="83.5" fill="#FDC04C"/>
                     </g>
-                <g id="sun_radiant">
-                    <path id="Line 10" d="M227 227L255.991 255.991" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 7" d="M46.5 152H5.5" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 6" d="M77.4002 226.6L48.4089 255.591" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 5" d="M152 257.5V298.5" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 9" d="M76.9914 77.9914L48 49" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 3" d="M257.5 152H298.5" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 2" d="M226.6 77.4002L255.591 48.4089" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                    <path id="Line 1" d="M152 46.5V5.5" stroke="#BA3902" stroke-opacity="0.98" stroke-width="10" stroke-linecap="round"/>
-                </g>
+                    <g id="sun_radiant">
+                        <path id="Line 10" d="M255.991 255.991L227 227" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 7" d="M5.5 152L46.5 152" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 6" d="M48.4089 255.591L77.4002 226.6" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 5" d="M152 298.5V257.5" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 9" d="M48 49L76.9914 77.9914" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 3" d="M298.5 152H257.5" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 2" d="M255.591 48.4089L226.6 77.4002" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                        <path id="Line 1" d="M152 5.5V46.5" stroke="#BA3902" strokeOpacity="0.98" strokeWidth="10" strokeLinecap="round"/>
+                    </g>
                 <g id="arms">
                     <path id="left_arm" d="M58 152C76.8404 167.5 79.8404 169.5 100.5 184.5" stroke="black" strokeWidth="5" strokeLinecap="round"/>
                     <path id="right_arm" d="M246.84 152C229.84 166 226.84 168.5 204.34 184.5" stroke="black" strokeWidth="5" strokeLinecap="round"/>
@@ -122,9 +155,15 @@ const CoolSun: React.FC<CoolSunProps> = ({isAnimated}) => {
                             <path id="right_2" d="M172 140H177" stroke="black" strokeWidth="5" strokeLinecap="round"/>
                         </g>
                     </g>
+                    <g style={{
+                        transformOrigin: "50% 50%",
+                    }} id="eyes_opened">
+                        <circle id="left_open_eye" cx="130.75" cy="139.75" r="7.75" fill="black"/>
+                        <circle id="right_open_eye" cx="171.75" cy="139.75" r="7.75" fill="black"/>
+                    </g>
                     <g id="mouth">
-                        <path id="left_3" d="M152 153H147" stroke="black" stroke-width="5" stroke-linecap="round"/>
-                        <path id="right_3" d="M152 153H157" stroke="black" stroke-width="5" stroke-linecap="round"/>
+                        <path id="left_3" d="M152 153H147" stroke="black" strokeWidth="5" strokeLinecap="round"/>
+                        <path id="right_3" d="M152 153H157" stroke="black" strokeWidth="5" strokeLinecap="round"/>
                     </g>
                 </g>
             </g>
