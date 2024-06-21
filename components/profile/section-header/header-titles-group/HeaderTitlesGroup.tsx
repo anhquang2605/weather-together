988581e2485e5c8f3aa8838a9c363636a7089e65
Anh Quang Chu <anhquang2605@gmail.com>
2024-06-20 18:04:26 -0700

    setting up backbones for the section-header, especially the header bar and tittle group

--- components/profile/section-header/header-titles-group/HeaderTitlesGroup.tsx
+++ components/profile/section-header/header-titles-group/HeaderTitlesGroup.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import style from './header-titles-group.module.css';
 
 interface HeaderTitlesGroupProps {
-    titles: string[]
+    titles: string[];
+    currentIndex?: number;
 }
-
-const HeaderTitlesGroup: React.FC<HeaderTitlesGroupProps> = ({titles}) => {
+//HELPERS
+const transformTitle = (title: string) => {
+    return title.charAt(0).toUpperCase() + title.slice(1);
+}
+const HeaderTitlesGroup: React.FC<HeaderTitlesGroupProps> = ({titles, currentIndex = 0}) => {
     return (
         <div className={style['header-titles-group']}>
-            HeaderTitlesGroup
+            {transformTitle(titles[currentIndex])}
         </div>
     );
 };
