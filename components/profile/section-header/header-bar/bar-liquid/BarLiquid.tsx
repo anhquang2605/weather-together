59c0293dae6f26210d9e406b6bdae91d05922cc5
Anh Quang Chu <anhquang2605@gmail.com>
2024-07-15 13:16:45 -0700

    figure out the direction for bar liquid

--- components/profile/section-header/header-bar/bar-liquid/BarLiquid.tsx
+++ components/profile/section-header/header-bar/bar-liquid/BarLiquid.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import style from './bar-liquid.module.css';
 
 interface BarLiquidProps {
-
+    progress?: number // 0 - 1
+    side?: 'left' | 'right'
 }
 
-const BarLiquid: React.FC<BarLiquidProps> = ({}) => {
+const BarLiquid: React.FC<BarLiquidProps> = ({progress = 0, side = 'left'}) => {
     return (
-        <div className={style['bar-liquid']}>
+        <div className={style['bar-liquid'] + ' ' + style[side]}>
             
         </div>
     );
