a3238b93169aa69604f0fb2e82a0c4719427c279
anhquang2605 <anhquang2605@gmail.com>
2024-12-04 15:30:17 -0800

    defined colors for each weather type

--- constants/weathers.ts
+++ constants/weathers.ts
@@ -30,7 +30,7 @@ export const weatherToColorClassMap:weathersMap = {
     "rainy": "text-indigo-400" ,
     "snowy": "text-blue-400",
     "thunder": "text-yellow-400" ,
-    "windy": "text-green-400" ,
+    "windy": "text-lime-400" ,
 }
 export const weatherNameToIconAliasMap:weathersMap = {
     "sunny": "sun",
@@ -39,4 +39,13 @@ export const weatherNameToIconAliasMap:weathersMap = {
     "snowy": "snowflake",
     "thunder": "bolt" ,
     "windy": "wind" ,
+}
+
+export const weatherToColor: weathersMap = {
+    "sunny": "#fb923c",
+    "cloudy": "#38bdf8",
+    "rainy": "#818cf8",
+    "snowy": "#60a5fa",
+    "thunder": "#facc15",
+    "windy": "#a3e635",
 }
