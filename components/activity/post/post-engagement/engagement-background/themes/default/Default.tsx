ded3020f66470d23598024185a7ca14ffb4080af
anhquang2605 <anhquang2605@gmail.com>
2025-01-17 10:21:01 -0800

    added svg files to engagement background

--- components/activity/post/post-engagement/engagement-background/themes/default/Default.tsx
+++ components/activity/post/post-engagement/engagement-background/themes/default/Default.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import style from './default.module.css';
+import SVGWrapper from '../../../svg-wrapper/SVGWrapper';
 
 interface DefaultProps {
 
+}
+const SVGFILES = [
+    'snow-cloud',
+    'rain-cloud',
+    'sun-cloud',
+    'thunder-cloud',
+    'rb-cloud',
+]
+const SVGWrapperGenerator = (fileNames: string[]) => {
+    const backbones = [];
+    for (const fileName of fileNames) {
+        const theSVG = <SVGWrapper fileName={fileName} />
+        backbones.push(theSVG);
+    }
+
+    return backbones;
 }
 
 const Default: React.FC<DefaultProps> = ({}) => {
     return (
         <div className={style['default']}>
-            Default
+            {
+                SVGWrapperGenerator(SVGFILES)
+            }   
         </div>
     );
 };
--- components/activity/post/post-engagement/svg-wrapper/SVGWrapper.tsx
+++ components/activity/post/post-engagement/svg-wrapper/SVGWrapper.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './.module.css';
 
 interface SVGWrapperProps {
-    svgFileName: string;
+    fileName: string;
 }
 
 const SVGWrapper: React.FC<SVGWrapperProps> = (props) => {
     const {
-        svgFileName
+        fileName
     } = props;
-    const SvgComponent = require(`./../../../../assets/svg/post-engagement/${svgFileName}.svg`).default;
+    const SvgComponent = require(`./../../../../assets/svg/post-engagement/${fileName}.svg`).default;
     return (
         <div className={style['svg-wrapper']}>
             <SvgComponent />
