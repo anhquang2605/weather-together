312ec8e7a62649516b55c15673231d4a1e25af22
anhquang2605 <anhquang2605@gmail.com>
2025-01-20 16:13:43 -0800

    added stages

--- components/plugins/animations/useLinearAnimation/useLinearAnimation.tsx
+++ components/plugins/animations/useLinearAnimation/useLinearAnimation.tsx
@@ -1,24 +1,40 @@
 import React, {useEffect, useState, useContext} from 'react'
-
+import anime from 'animejs';
 interface LinearAnimationContextType{
     duration: number,
     delay: number,
     repeatCount: number
     isRepeating: boolean,
+    setDuration: React.Dispatch<React.SetStateAction<number>>,
+    setDelay: React.Dispatch<React.SetStateAction<number>>,
+    setRepeatCount: React.Dispatch<React.SetStateAction<number>>,
+    setIsRepeating: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const LinearAnimationContext = React.createContext<LinearAnimationContextType>({
-    duration: 0,
-    delay: 0,
-    repeatCount: 0,
-    isRepeating: false
-})
+type LinearAnimationStage ={
+    duration: number;
+    delay: number;
+    playAnimation: () => void;
+    callback: () => void;
+    
+}
+const LinearAnimationContext = React.createContext<LinearAnimationContextType | null>( null)
 
 const LinearAnimationProvider = ({children}:any) => {
     const [duration, setDuration] = useState(0);
     const [delay, setDelay] = useState(0);
     const [repeatCount, setRepeatCount] = useState(0);
     const [isRepeating, setIsRepeating] = useState(false);
+    const [stages, setStages] = useState<LinearAnimationStage[]>([]);
+    const addStage = (stage: LinearAnimationStage) => {
+        setStages([...stages, stage]);
+    }
+    const playStages = () => {
+        stages.forEach((stage) => {
+            stage.playAnimation();
+            stage.callback();
+        })
+    }
     
     const value = {
         duration,
@@ -42,3 +58,5 @@ export const useLinearAnimation = () => {
     if(!context) throw new Error('useLinearAnimation must be used within a LinearAnimationProvider');
     return context
 } 
+
+export default LinearAnimationProvider
