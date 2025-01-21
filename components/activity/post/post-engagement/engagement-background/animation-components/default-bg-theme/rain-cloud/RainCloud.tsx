6551a1be80a5db0554c04cc8560c91ce3273fe1a
anhquang2605 <anhquang2605@gmail.com>
2025-01-21 15:42:44 -0800

    working on the svg cloud

--- components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/rain-cloud/RainCloud.tsx
+++ components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/rain-cloud/RainCloud.tsx
@@ -3,25 +3,32 @@ import style from './rain-cloud.module.css';
 import SVGWrapper from '../../../../svg-wrapper/SVGWrapper';
 import { pathRevealAnimation, pathShrinkAnimation } from '../../../../../../../../libs/anime-animations-helpers'; 
 import { SVGCloudPropType } from '../svg-cloud-types';
+import anime from 'animejs';
 
 const RainCloud: React.FC<SVGCloudPropType> = (props) => {
     const {
         duration = 3000,
         delay = 0,
-        easing = 'easeOut'
+        easing = 'linear'
     } = props
 
     const targets =  '#rain-drops path';
     const startAnimation = () => {
-        const anim = pathRevealAnimation(targets, 'easeOut', 3000);
-        anim.play();
+  /*       const anim = pathRevealAnimation(targets, easing, duration); */
+        const anim2 = anime({
+            targets: targets,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: easing,
+            duration: duration,
+        })
         
     }
     useEffect(()=> {
-        const rainDropsElements = document.getElementById('rain-drops');
-        if(rainDropsElements) {
+  /*       const rainDropsElements = document.getElementById('rain-drops');
+        console.log(rainDropsElements);
+        if(rainDropsElements) { */
             startAnimation();
-        }
+/*         } */
     },[])
     return (
         <div className={style['rain-cloud']}>
