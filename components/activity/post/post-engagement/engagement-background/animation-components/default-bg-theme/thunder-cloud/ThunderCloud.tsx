ae96583d0dcf990e5e8a1b3467a93b657088de03
anhquang2605 <anhquang2605@gmail.com>
2025-01-28 14:54:24 -0800

    finished the thunder cloud animation

--- components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/thunder-cloud/ThunderCloud.tsx
+++ components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/thunder-cloud/ThunderCloud.tsx
@@ -18,26 +18,38 @@ const ThunderCloud: React.FC<ThunderCloudProps> = (props) => {
     const CLOUD_BIG_SCALE = 1;
     const FLASH_COLOR = '#f7ff00'
     const CLOUD_COLOR = '#222';
-    const CLOUD_EXPAND_DURATION_PERCENT = 0.7;
-    const THUNDER_STRIKE_DURATION_PERCENT = 0.2;
+    const CLOUD_EXPAND_DURATION_PERCENT = 0.85;
+    const THUNDER_STRIKE_DURATION_PERCENT = 1 - CLOUD_EXPAND_DURATION_PERCENT;
+   
+
     const startAnimation = () => {
+        //setting up variables
+        const cloudExpandDuration = duration * CLOUD_EXPAND_DURATION_PERCENT;
+        const thunderStrikeDuration = duration * THUNDER_STRIKE_DURATION_PERCENT;
+        const THUNDER_CLOUD_AND_FACE_SELECTOR = `#${style['thunder-cloud']} , #${style['thunder-cloud-face']}`
         const timeline = anime.timeline();
         //Cloud expand stage
-        const cloudExpandAnim: any = propertiesStagesAnimation("#" +style['thunder-cloud'], easing, duration * CLOUD_EXPAND_DURATION_PERCENT, {scale: [0.5, 1]}, false);
+        const cloudExpandAnim: any = propertiesStagesAnimation(THUNDER_CLOUD_AND_FACE_SELECTOR, easing, duration * CLOUD_EXPAND_DURATION_PERCENT, {scale: [0.5, 1]}, false);
         timeline.add(cloudExpandAnim);
         //Thunder strike stage
-        const thunderStrikeAnim: any = pathRevealAnimation('#thunder-strike path', easing, duration * THUNDER_STRIKE_DURATION_PERCENT, false);
-        thunderStrikeAnim.delay = duration * CLOUD_EXPAND_DURATION_PERCENT;
+
+        const thunderStrikeAnim: any = pathRevealAnimation('#thunder-strike path', easing, thunderStrikeDuration, false);
         //thunder cloud shrink and blink
-        const thunderCloudShrinkandBlinkAnim: any = propertiesStagesAnimation("#" +style['thunder-cloud'], 'easeOutElastic(2, 0.4)', duration * THUNDER_STRIKE_DURATION_PERCENT, 
+        const thunderCloudShrinkandBlinkAnim: any = propertiesStagesAnimation(THUNDER_CLOUD_AND_FACE_SELECTOR, 'easeOutElastic(1,0.5)', duration * THUNDER_STRIKE_DURATION_PERCENT, 
             {
                 scale: [1,0.8],
-                fill: [FLASH_COLOR, CLOUD_COLOR]
+               
             }
             , false);
-        thunderCloudShrinkandBlinkAnim.delay = duration * CLOUD_EXPAND_DURATION_PERCENT;
-        timeline.add(thunderCloudShrinkandBlinkAnim);
-        timeline.add(thunderStrikeAnim, '-=' + duration * THUNDER_STRIKE_DURATION_PERCENT);
+        
+        const thunderCloudBrinkAnim: any = propertiesStagesAnimation("#" +style['thunder-cloud'], 'linear', duration * THUNDER_STRIKE_DURATION_PERCENT,
+            {
+                fill: [CLOUD_COLOR,FLASH_COLOR, CLOUD_COLOR]
+            }
+            , false);
+        timeline.add(thunderCloudBrinkAnim, cloudExpandDuration);
+        timeline.add(thunderCloudShrinkandBlinkAnim, cloudExpandDuration);
+        timeline.add(thunderStrikeAnim,  cloudExpandDuration);
     }
     useEffect(() => {
         startAnimation();
@@ -56,7 +68,7 @@ const ThunderCloud: React.FC<ThunderCloudProps> = (props) => {
             <path d="m73.967 52.645-5.8494 6.7929 10.661 3.3021-6.4155 3.6795 1.6039 2.0756" stroke-width="1.323"/>
         </g>
         <path d="m87.088 34.607c0.09183-0.48812 0.13972-0.99176 0.13972-1.5068 0-4.4271-3.543-8.016-7.9136-8.016-2.6741 0-5.0385 1.3436-6.4709 3.4007-1.3753-1.1695-3.1491-1.8738-5.0851-1.8738-4.3286 0-7.8455 3.5203-7.9126 7.8887h-9.96e-4c-4.8561 0-8.7927 3.9875-8.7927 8.9066 0 4.919 3.9366 8.9066 8.7927 8.9066h25.876c4.856 0 8.7927-3.9876 8.7927-8.9066 0-4.4478-3.2185-8.1339-7.4251-8.7994z" fill="#222" stroke-width=".52363" id={style["thunder-cloud"]}/>
-        <g stroke="#aaa4a4" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3229">
+        <g stroke="#aaa4a4" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3229" id={style["thunder-cloud-face"]}>
             <g transform="translate(2.2844)" fill="#202020" >
                 <path d="m63.306 42.978 5.0003 2.3586"/>
                 <path d="m77.69 42.978-5.0003 2.3586"/>
