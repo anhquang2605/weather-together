041ee53a18837ebba66430e5ba2cc8508855be4d
anhquang2605 <anhquang2605@gmail.com>
2025-01-31 08:31:52 -0800

    adjusted transform origin for each snow flake

--- components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/snow-cloud/SnowCloud.tsx
+++ components/activity/post/post-engagement/engagement-background/animation-components/default-bg-theme/snow-cloud/SnowCloud.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './snow-cloud.module.css';
+import { SVGCloudPropType } from '../svg-cloud-types';
+import anime from 'animejs';
+import { propertiesStagesAnimation } from '../../../../../../../../libs/anime-animations-helpers';
 
-interface SnowCloudProps {
+interface SnowCloudProps extends SVGCloudPropType {
 
 }
 
-const SnowCloud: React.FC<SnowCloudProps> = ({}) => {
+const SnowCloud: React.FC<SnowCloudProps> = (props) => {
+    const {duration = 2000, delay = 0, easing = 'linear'} = props
+    const startAnimation = () => {
+        const timeline = anime.timeline({ delay: delay, loop: true});
+        //snow flake animation
+        const snowFlakeAnim:any = propertiesStagesAnimation('.' + style['snow-flake'], easing, duration, 
+            {
+                translateY: [-5, 0],
+                scale: [0, 1]
+            }, 
+        false);
+        //cloud-body animation
+
+        //cloud animation
+        
+        //adding animation
+        timeline.add(snowFlakeAnim);
+
+    }   
+    useEffect(() => {
+        startAnimation();       
+    },[])
     return (
         <div className={style['snow-cloud']}>
             <svg width="44.63mm" height="30.366mm" version="1.1" viewBox="0 0 44.63 30.366" xmlns="http://www.w3.org/2000/svg">
@@ -20,19 +44,19 @@ const SnowCloud: React.FC<SnowCloudProps> = ({}) => {
                 </g>
   
                 <g id="snow-flakes" fill="#202020" stroke="#00adce" stroke-linecap="round" stroke-width="1.3229">
-                    <g className={style['snow-flake']}>
+                    <g id={style['snow-flake-1']} className={style['snow-flake']}>
                         <path d="m58.578 46.25v8.5392"/>
                         <path d="m54.309 50.519h8.5392"/>
                         <path d="m55.571 47.403 6.009 6.0671"/>
                         <path d="m61.421 47.405-6.009 6.0671"/>
                     </g>
-                    <g className={style['snow-flake']}>
+                    <g id={style['snow-flake-2']} className={style['snow-flake']}>
                         <path d="m90.75 33.581v8.5392"/>
                         <path d="m86.481 37.85h8.5392"/>
                         <path d="m87.743 34.734 6.009 6.0671"/>
                         <path d="m93.593 34.736-6.009 6.0671"/>
                     </g>
-                    <g className={style['snow-flake']}>
+                    <g id={style['snow-flake-3']} className={style['snow-flake']}>
                         <path d="m71.14 28.818v8.5392"/>
                         <path d="m66.871 33.087h8.5392"/>
                         <path d="m68.133 29.971 6.009 6.0671"/>
