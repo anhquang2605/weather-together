796ca71fea5b44214b6a61a556c90dc1c524f45d
anhquang2605 <anhquang2605@gmail.com>
2025-01-31 10:46:33 -0800

    added a shuffler and a number generator

--- components/activity/post/post-engagement/engagement-background/themes/default/Default.tsx
+++ components/activity/post/post-engagement/engagement-background/themes/default/Default.tsx
@@ -38,20 +38,34 @@ const SVG_CLOUD_GENERATOR = (CloudComponents: JSX.Element[]) => {
 }
 const randomizedOrders = (maxRange: number) => {
     const numbers = [];
-    for (let i = maxRange; i > 0; i--) {
-        const j = Math.floor(Math.random() * i);
+    for (let i = maxRange - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i+1));
         numbers[j] = i;
-        numbers[i - 1] = j;
+        numbers[i] = j;
     }
     return numbers;
 }
-useEffect(() => {
-    const orders = randomizedOrders(CloudComponents.length);
-    console.log(orders);
-})
-
+//Determine the position to add to an array, this way each position will be unique, and the order will be randomized
+function generateAndShuffleRange(start: number, end: number) {
+    let arr: number[] = [];
+    for (let i = start; i <= end; i++) {
+        // Generate a random index to insert the current number
+        //get j between 0 and arr.length and 1, this is math floor so gotta add 1 to the max range to include end. as the array grow, the max range will increase as well.
+        const j = Math.floor(Math.random() * (arr.length + 1));
+        // Insert the number at the random position
+        //start position j, remove 0 element, add i to this position
+        arr.splice(j, 0, i);
+    }
+    return arr;
+}
 const CLOUD_DELAY = 2000;
 const Default: React.FC<DefaultProps> = ({}) => {
+    useEffect(() => {
+        //const orders = randomizedOrders(CloudComponents.length);
+        const orders = generateAndShuffleRange(0, CloudComponents.length - 1);
+        console.log(orders);
+    },[])
+    
     return (
         <div className={style['default']}>
             <div className={style['svg-cloud']}>
