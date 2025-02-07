6bfb243f009b738c5942f9e1945db6ee880e38b6
anhquang2605 <anhquang2605@gmail.com>
2025-02-07 11:30:58 -0800

    added the spinner

--- components/weather-spinner/WeatherSpinner.tsx
+++ components/weather-spinner/WeatherSpinner.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './weather-spinner.module.css';
+import anime from 'animejs';
 
 interface WeatherSpinnerProps {
 
 }
 
 const WeatherSpinner: React.FC<WeatherSpinnerProps> = ({}) => {
+    const startAnimation = () => {
+        const timeline = anime.timeline({});
+    }
+    useEffect(()=> {
+
+    },[])
     return (
         <div className={style['weather-spinner']}>
             WeatherSpinner
