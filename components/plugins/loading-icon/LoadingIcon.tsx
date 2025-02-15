334d37b64c52c512e93bdff23162715bef168530
Anh Quang Chu <anhquang2605@gmail.com>
2025-02-15 13:23:06 -0800

    working on animation

--- components/plugins/loading-icon/LoadingIcon.tsx
+++ components/plugins/loading-icon/LoadingIcon.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
 import style from './loading-icon.module.css';
 import anime from 'animejs';
+import { propertiesStagesAnimation } from '../../../libs/anime-animations-helpers';
 interface LoadingIconProps {
 
 }
 
 const LoadingIcon: React.FC<LoadingIconProps> = ({}) => {
     const startAnimation = () => {
-
-    
+       const anim = anime({
+           targets: '.' + style['loading-icon__circle'],
+           scale: [1, 1.45, 1],
+           backgroundColor: ['#fff ','#B1F4E7', '#fff'],
+           duration: 3000,
+           loop: true,
+                      delay: anime.stagger(200)
+       });
+       
     }
     useEffect(()=>{
-
+        startAnimation();
     }, [])
     return (
         <div className={style['loading-icon']}>
