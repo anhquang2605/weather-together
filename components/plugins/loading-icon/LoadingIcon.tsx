74df759e3704a878d1e769fe32678be2ba273230
Anh Quang Chu <anhquang2605@gmail.com>
2025-02-18 18:16:36 -0800

    finished loading icon

--- components/plugins/loading-icon/LoadingIcon.tsx
+++ components/plugins/loading-icon/LoadingIcon.tsx
@@ -3,28 +3,34 @@ import style from './loading-icon.module.css';
 import anime from 'animejs';
 import { propertiesStagesAnimation } from '../../../libs/anime-animations-helpers';
 interface LoadingIconProps {
-
+    staggerDelay?: number
+    duration?: number
 }
-const STAGGER_DELAY = 250;
-const LoadingIcon: React.FC<LoadingIconProps> = ({}) => {
+const STAGGER_DELAY = 350;
+const DURATION = 2000;
+const LoadingIcon: React.FC<LoadingIconProps> = ({
+    staggerDelay = STAGGER_DELAY,
+    duration = DURATION
+}) => {
     const startAnimation = () => {
        const anim = anime({
            targets: '.' + style['loading-icon__circle'],
-           scale: [0.8, 1.5, 0.8],
-           duration: 3000,
+           scale: [1, 1.75, 1],
+           backgroundColor: ['#ffffff','#615FFF', '#ffffff'],
+           duration: duration,
            direction: '',
            easing: 'linear',
            loop: true,
                       delay: anime.stagger(STAGGER_DELAY)
        });
-       const anim2 = anime({
+/*        const anim2 = anime({
            targets: '.' + style['loading-icon__circle'],
-           backgroundColor: ['#ffffff','#615FFF', '#ffffff'],
-           duration: 3000,
+          
+           duration: duration,
            easing: 'linear',
            loop: true,
                       delay: anime.stagger(STAGGER_DELAY)
-       })
+       }) */
        
     }
     useEffect(()=>{
