6b5aa6056063325efcffdefe9999613ba038fc40
Anh Quang Chu <anhquang2605@gmail.com>
2025-03-12 14:43:05 -0700

    changed path drawing d

--- components/weather-spinner/animations/Sunny/Sunny.tsx
+++ components/weather-spinner/animations/Sunny/Sunny.tsx
@@ -53,17 +53,17 @@ const Sunny: React.FC<SunnyProps> = ({}) => {
     },[])
     return (
         <div className={style['sunny']}>
-            <svg width="51.734mm" height="51.734mm" version="1.1" viewBox="0 0 51.734 51.734" xmlns="http://www.w3.org/2000/svg">
+            <svg width="60mm" height="60mm" version="1.1" viewBox="0 0 60 60"  xmlns="http://www.w3.org/2000/svg">
                 <g stroke-linecap="round" stroke-width="1.3229">
                 <g  id={style["sunny-sun_path"]}  fill="none" stroke="#ffc60b">
-                    <path d="m42.995 25.867h8.0773"/>
-                    <path d="m37.978 37.978 5.7115 5.7115"/>
-                    <path d="m25.867 42.995v8.0773"/>
-                    <path d="m13.756 37.978-5.7115 5.7115"/>
-                    <path d="m8.739 25.867h-8.0773"/>
-                    <path d="m13.756 13.756-5.7115-5.7115"/>
-                    <path d="m25.867 8.7388v-8.0773"/>
-                    <path d="m37.978 13.756 5.7115-5.7115"/>
+                <path d="m42.995 25.867h8.0773"/>
+   <path d="m37.978 37.978 5.7115 5.7115"/>
+   <path d="m25.867 42.995v8.0773"/>
+   <path d="m13.756 37.978-5.7115 5.7115"/>
+   <path d="m8.739 25.867h-8.0773"/>
+   <path d="m13.756 13.756-5.7115-5.7115"/>
+   <path d="m25.867 8.7388v-8.0773"/>
+   <path d="m37.978 13.756 5.7115-5.7115"/>
                 </g>
                     <circle id={style["sunny-sun_stroke"]} cx="25.867" cy="25.867" r="12.111" fill="none" stroke="#000" />
                     <circle id={style["sunny-filled_sun"]} cx="25.867" cy="25.848" r="12.111" fill="#fff57e" stroke="#ffc60b" />
