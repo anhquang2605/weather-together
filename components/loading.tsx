d3d0d8fe720e5b825c9d0b72f2c8d401e70b7b15
Anh Quang Chu <anhquang2605@gmail.com>
2025-05-14 11:42:23 -0700

    added start update

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,9 +1,13 @@
 import WeatherSpinner from "./weather-spinner/WeatherSpinner";
 
-export default function Loading() {
+type LoadingProps = {
+    startUpdate?: boolean;
+}
+
+const Loading: React.FC<LoadingProps> = ({ startUpdate }: LoadingProps) => {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full glass fixed top-0 left-0 loading-screen">
-            <WeatherSpinner />  
+            <WeatherSpinner startUpdate={startUpdate} />  
         </div>
     )
 }
--- pages/tester.tsx
+++ pages/tester.tsx
@@ -20,12 +20,18 @@ export default function Tester(userJSON:any) {
     //const user:User = JSON.parse(userJSON);
     //const posts = generateRandomPosts(10);
     const [posts, setPosts] = useState([]);
+    const [startUpdate, setStartUpdate] = useState(false);
     const profilePicturePaths = {
         'anhquang2605': faker.image.avatar(),
         'chuquang2605': faker.image.avatar(),
     }
+    const interval = 1000;
+    const mockTimeEvent = (interval: number) => {
+        const theinterval = setInterval(() => {
+            setStartUpdate((prev) => !prev);
+        }, interval);
 
-
+    }
         useEffect(()=>{
   /*           let text = "About Me";
             let characters = text.split("").map(char => `<span>${char}</span>`).join("");
@@ -56,9 +62,10 @@ export default function Tester(userJSON:any) {
                     }
                 }
             } */
-        getPostByUsernamesString('anhquang2605').then((posts) => {
-                setPosts(posts)
-        })
+            getPostByUsernamesString('anhquang2605').then((posts) => {
+                    setPosts(posts)
+            })
+            mockTimeEvent(interval);
         },[])
 /*     useEffect(() => {
      const svg = document.getElementById(style['wave']);
@@ -82,7 +89,7 @@ export default function Tester(userJSON:any) {
         <MockContext.Provider value={{profilePicturePaths}}>
         <div className="w-full h-full overflow-y-scroll">
             <h1>Tester</h1>
-            <Loading/>
+            <Loading startUpdate/>
         <div>
 
         </div>
