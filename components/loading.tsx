bc219cb5b180a65cc73fc45ad55bb7f102201610
Anh Quang Chu <anhquang2605@gmail.com>
2025-05-15 12:47:58 -0700

    revamp the loading component

--- components/loading.tsx
+++ components/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
     const [startUpdate, setStartUpdate] = useState(false);
     return (
         <div className="flex flex-col justify-center items-center w-full h-full glass fixed top-0 left-0 loading-screen">
-            <WeatherSpinner startUpdate={startUpdate} />  
+            <WeatherSpinner />  
         </div>
     )
 }
--- components/weather-spinner/WeatherSpinner.tsx
+++ components/weather-spinner/WeatherSpinner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import style from './weather-spinner.module.css';
 import anime from 'animejs';
 import Cloudy from './animations/cloudy/Cloudy';
@@ -6,25 +6,48 @@ import Sunny from './animations/sunny/Sunny';
 import Windy from './animations/windy/Windy';
 
 interface WeatherSpinnerProps {
-    startUpdate?: boolean;
 }
 
-const WeatherSpinner: React.FC<WeatherSpinnerProps> = ({startUpdate = false}) => {
+const WeatherSpinner: React.FC<WeatherSpinnerProps> = () => {
     const [startRain, setStartRain] = useState(false);
     const [startWind, setStartWind] = useState(false);
     const [startSun, setStartSun] = useState(false);
+    const [rainFinished, setRainFinished] = useState(true);
+    const [windFinished, setWindFinished] = useState(true);
+    const [sunFinished, setSunFinished] = useState(true);
     const startAnimation = () => {
         const timeline = anime.timeline({});
     }
 
     useEffect(()=> {
-        setStartRain(startUpdate );
+        setStartRain(true);
     },[])
+    useEffect(() => {
+        if(sunFinished){
+            setWindFinished(false);
+            setStartRain(true);
+            setStartSun(false);
+        }
+    }, [sunFinished]);
+    useEffect(() => {
+        if(rainFinished){
+            setSunFinished(false);
+            setStartWind(true);
+            setStartRain(false);
+        }
+    }, [rainFinished]);
+    useEffect(() => {
+        if(windFinished){
+            setRainFinished(false);
+            setStartSun(true);
+            setStartWind(false);
+        }
+    }, [windFinished]);
     return (
         <div className={style['weather-spinner']}>
-            <Cloudy toStart={startUpdate}/>
-            <Sunny toStart={startUpdate}/>
-            <Windy toStart={startUpdate}/>
+            <Cloudy toStart={startRain}/>
+            <Sunny toStart={startSun}/>
+            <Windy toStart={startWind}/>
         </div>
     );
 };
