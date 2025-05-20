c36eca74dd80cb162e2572dc894d8b6cf79c2a63
Anh Quang Chu <anhquang2605@gmail.com>
2025-05-20 11:02:17 -0700

    readded the true API getter

--- pages/weatherContext.tsx
+++ pages/weatherContext.tsx
@@ -23,12 +23,12 @@ export const WeatherContextProvider = ({children}: WeatherContextProviderProps) 
     const [todayWeather, setTodayWeather] = useState<Value | null>(null);//[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
     const [weeklyWeather, setWeeklyWeather] = useState<Value[]>([]);
     const getWeatherData = async (location: string) => {
-        const weatherData = require('../data/mock-weather.json');
+/*         const weatherData = require('../data/mock-weather.json');
         const data:any = Object.values(weatherData.locations)[0];
         setTodayWeather(data.values[0]);
         setCurWeather(data.currentConditions);
-        setWeeklyWeather(data.values);
-/*         getWeather(location).then((data) => {
+        setWeeklyWeather(data.values); */
+        getWeather(location).then((data) => {
             if(data){
                 const locations = Object.values(data.locations);
                 const location = locations[0];
@@ -37,7 +37,7 @@ export const WeatherContextProvider = ({children}: WeatherContextProviderProps) 
             }
         }).catch((err) => {
             console.log(err)
-        }) */
+        })
     }
     
     return (
