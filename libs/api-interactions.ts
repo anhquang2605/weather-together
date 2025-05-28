32690d9416b29b87e6dad5cd177f250766fd7b8b
Anh Quang Chu <anhquang2605@gmail.com>
2025-05-28 10:48:41 -0700

    removed local host environment path in api lib

--- libs/api-interactions.ts
+++ libs/api-interactions.ts
@@ -1,5 +1,5 @@
 export function fetchFromGetAPI (path: string, options: any){
-    const baseURL = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+    const baseURL = process.env.NEXTAUTH_URL ;
     const url = `${baseURL}/api/${path}`;
     const params = new URLSearchParams(options).toString();
     return fetch(`${url}?${params}`, {}).then(res => {
@@ -12,7 +12,7 @@ export function fetchFromGetAPI (path: string, options: any){
 }
 
 export function updateToPutAPI(path: string, body: any){
-    const baseURL = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+    const baseURL = process.env.NEXTAUTH_URL ;
     const url = `${baseURL}/api/${path}`;
     return fetch(url, {
         method: "PUT",
@@ -30,7 +30,7 @@ export function updateToPutAPI(path: string, body: any){
 }
 
 export function insertToPostAPI(path: string, body: any){
-    const baseURL = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+    const baseURL = process.env.NEXTAUTH_URL;
     const url = `${baseURL}/api/${path}`;
     return fetch(url, {
         method: "POST",
@@ -47,7 +47,7 @@ export function insertToPostAPI(path: string, body: any){
     });
 }
 export function uploadFileToPostAPI(path: string, file: any){
-    const baseURL = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+    const baseURL = process.env.NEXTAUTH_URL ;
     const url = `${baseURL}/api/${path}`;
     const formData = new FormData();
     formData.append('file', file);
@@ -63,7 +63,7 @@ export function uploadFileToPostAPI(path: string, file: any){
     });
 }
 export function deleteFromDeleteAPI(path: string, params: any){
-    const baseURL = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+    const baseURL = process.env.NEXTAUTH_URL ;
     const url = `${baseURL}/api/${path}`;
     const paramsString = new URLSearchParams(params).toString();
     return fetch(`${url}?${paramsString}`, {
