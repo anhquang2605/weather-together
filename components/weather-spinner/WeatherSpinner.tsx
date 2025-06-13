d904b1fa5ece6e16a6cb062e908dd06f907b3850
Anh Quang Chu <anhquang2605@gmail.com>
2025-06-13 11:25:15 -0700

    removed unused

--- components/weather-spinner/WeatherSpinner.tsx
+++ components/weather-spinner/WeatherSpinner.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './weather-spinner.module.css';
 import anime from 'animejs';
 import Cloudy from './animations/Cloudy/Cloudy.tsx';
